ca08ce34546559440dccc36c0827c5bb32aa8986
Bartosz Białecki <bartosz.bialecki@gmail.com>
2021-03-05 15:03:37 +0100

    Wrap application in redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider as ReduxProvider } from 'react-redux'
+
+import configureStore from './state/store';
 import reportWebVitals from './reportWebVitals';
+
+import App from './App';
+
+import './index.css';
+
+const store = configureStore()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={ store }>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
