571091d1255004f1ea4c7838164b6b9b73b67968
Bartosz Białecki <bartosz.bialecki@gmail.com>
2021-03-05 15:01:35 +0100

    Add redux reducers

---
+++ src/state/reducers.ts
@@ -0,0 +1,57 @@
+import {
+  CharacterActions,
+  CharacterActionTypes,
+  CharactersState,
+} from "./types";
+
+const initialState: CharactersState = {
+  characters: {
+    count: 0,
+    hasMoreData: false,
+    data: [],
+  },
+  loading: false,
+  error: null,
+  page: 1,
+};
+
+const reducer = (
+  state: CharactersState = initialState,
+  action: CharacterActions
+): CharactersState => {
+  switch (action.type) {
+    case CharacterActionTypes.FETCH_CHARACTERS:
+      return {
+        ...state,
+        loading: true,
+        characters:
+          action.payload.page === 1
+            ? { ...state.characters, data: [] }
+            : state.characters,
+      };
+    case CharacterActionTypes.FETCH_CHARACTERS_SUCCESS:
+      const { characters } = action.payload;
+      const nextPage = characters.hasMoreData ? state.page + 1 : 1;
+
+      return {
+        ...state,
+        loading: false,
+        page: nextPage,
+        characters: {
+          count: characters.count,
+          hasMoreData: characters.hasMoreData,
+          data: [...state.characters.data, ...characters.data],
+        },
+      };
+    case CharacterActionTypes.FETCH_CHARACTERS_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
