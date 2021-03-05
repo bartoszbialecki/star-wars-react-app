ce9e3ee1b5f593781fc865d0253db4b980c563a7
Bartosz Białecki <bartosz.bialecki@gmail.com>
2021-03-05 14:59:19 +0100

    Add types file containing all types used in the app

---
+++ src/state/types.ts
@@ -0,0 +1,63 @@
+export type ResourceUrl = string;
+export type ResourceId = number;
+
+export interface Resource {
+  id: number;
+}
+
+export interface PagedData<T> {
+  count: number;
+  hasMoreData: boolean;
+  data: T[];
+}
+
+export interface Planet extends Resource {
+  name: string;
+  population: number;
+}
+
+export interface Character extends Resource {
+  name: string;
+  homeworld: Planet;
+  filmIds: ResourceId[];
+}
+
+export interface CharactersState {
+  readonly characters: PagedData<Character>;
+  readonly loading: boolean;
+  readonly error: string | null;
+  readonly page: number;
+}
+
+export enum CharacterActionTypes {
+  FETCH_CHARACTERS = "@@characters/FETCH_CHARACTERS",
+  FETCH_CHARACTERS_SUCCESS = "@@characters/FETCH_CHARACTERS_SUCCESS",
+  FETCH_CHARACTERS_ERROR = "@@characters/FETCH_CHARACTERS_ERROR",
+}
+
+export interface FetchCharactersAction {
+  type: CharacterActionTypes.FETCH_CHARACTERS;
+  payload: {
+    searchValue: string;
+    page: number;
+  };
+}
+
+export interface FetchCharactersSuccessAction {
+  type: CharacterActionTypes.FETCH_CHARACTERS_SUCCESS;
+  payload: {
+    characters: PagedData<Character>;
+  };
+}
+
+export interface FetchCharactersErrorAction {
+  type: CharacterActionTypes.FETCH_CHARACTERS_ERROR;
+  payload: {
+    error: string;
+  };
+}
+
+export type CharacterActions =
+  | FetchCharactersAction
+  | FetchCharactersSuccessAction
+  | FetchCharactersErrorAction;
